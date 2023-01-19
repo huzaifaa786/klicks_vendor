f235e7b8d66d94ea3ad8ef2e4350d82f6f8a8268
Adil Awan <tritechuzaifa@gmail.com>
2023-01-19 11:18:22 +0500

    Adil, 19/1/2023

--- lib/screen/order_detail/order_detail.dart
+++ lib/screen/order_detail/order_detail.dart
@@ -31,62 +31,68 @@ class _OrderDetailState extends State<OrderDetail> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.only(left: 10, right: 10),
-        child: Column(
-          children: [
-            TitleTopbar(
-              text: 'Today Sale',
-              ontap: () {},
-            ),
-            TableCalendar(
-              selectedDayPredicate: (day) => isSameDay(day, today),
-              firstDay: DateTime.utc(2010, 10, 16),
-              lastDay: DateTime.utc(2030, 3, 14),
-              focusedDay: today,
-              onDaySelected: _onDaySelected,
-              calendarStyle: CalendarStyle(
-                markerDecoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                isTodayHighlighted: true,
-                outsideDaysVisible: false,
-              ),
-              availableCalendarFormats: const {
-                CalendarFormat.month: 'Month',
-              },
-              calendarFormat: format,
-              onFormatChanged: (CalendarFormat _format) {
-                setState(() {
-                  format = _format;
-                });
-              },
-              startingDayOfWeek: StartingDayOfWeek.sunday,
-              daysOfWeekVisible: true,
+          child: Column(
+        children: [
+          TitleTopbar(
+            text: 'Today Sale',
+            ontap: () {
+              Navigator.pop(context);
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10),
+            child: Column(
+              children: [
+                TableCalendar(
+                  selectedDayPredicate: (day) => isSameDay(day, today),
+                  firstDay: DateTime.utc(2010, 10, 16),
+                  lastDay: DateTime.utc(2030, 3, 14),
+                  focusedDay: today,
+                  onDaySelected: _onDaySelected,
+                  calendarStyle: CalendarStyle(
+                    markerDecoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    isTodayHighlighted: true,
+                    outsideDaysVisible: false,
+                  ),
+                  availableCalendarFormats: const {
+                    CalendarFormat.month: 'Month',
+                  },
+                  calendarFormat: format,
+                  onFormatChanged: (CalendarFormat _format) {
+                    setState(() {
+                      format = _format;
+                    });
+                  },
+                  startingDayOfWeek: StartingDayOfWeek.sunday,
+                  daysOfWeekVisible: true,
 
-              //Day Changed
+                  //Day Changed
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: TodaySale(
+                    imageicon: 'assets/images/car2.png',
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 1),
+                  child: TodaySale(
+                    imageicon: 'assets/images/car1.png',
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 1),
+                  child: TodaySale(
+                    imageicon: 'assets/images/car2.png',
+                  ),
+                )
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 20),
-              child: TodaySale(
-                imageicon: 'assets/images/car2.png',
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 1),
-              child: TodaySale(
-                imageicon: 'assets/images/car1.png',
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 1),
-              child: TodaySale(
-                imageicon: 'assets/images/car2.png',
-              ),
-            )
-          ],
-        ),
+          ),
+        ],
       )),
     );
   }
